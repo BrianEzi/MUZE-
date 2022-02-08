321d92ec370564d79b0558412bf044b30e2d6100
n80569fh <faiad.hussain@student.manchester.ac.uk>
2022-02-08 11:00:24 +0000

    search bar and nav

--- test.php
+++ test.php
@@ -1,12 +1,18 @@
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="myStyles.css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PHP Test</title>
+        <title>Muze#</title>
     </head>
     <body>
-        <?php 
-            echo '<p>Hello World</p>'; 
-        ?> 
+        <div class="topnav">
+            <a href="#chat">CHAT</a>
+            <a href="#games">GAMES</a>
+            <a href="#yourmusic">YOUR MUSIC</a>
+            <a href="#help">HELP</a>
+        </div>
+        <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+
     </body>
 </html>
