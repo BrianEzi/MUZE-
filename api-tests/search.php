ca038acee99e8c84a42ea604d44fd0edafb1c0fe
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-02-09 19:30:57 +0000

    api-tests: search: make list horizontal

--- api-tests/search.php
+++ api-tests/search.php
@@ -1,5 +1,29 @@
 <?php include_once "../api/doSearch.php"; ?>
 <?php if (isset($_GET["searchTerm"])) { ?>
+    <style>
+        .inline-list {
+            overflow-x: auto;
+            display: flex;
+            list-style-type: none;
+            padding-left: 1rem;
+            padding-right: 1rem;
+        }
+        .inline-list li {
+            display: inline-block;
+            padding: 1rem;
+        }
+        .inline-list li h1 {
+            width: 20vw;
+            text-overflow: ellipsis;
+            overflow: hidden;
+        }
+        .inline-list li img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 1rem;
+        }
+    </style>
+
     <?php $types = array_filter(SPOTIFY_CONTENT_TYPE::$ALL, function($type) { return array_key_exists($type, $_GET); }); ?>
     <ul>
         <li>Search terms: <?=$_GET["searchTerm"]?></li>
@@ -11,11 +35,11 @@
 
     <?php $results = doSearch($_GET["searchTerm"], $types); ?>
 
-    <ul>
+    <ul class="inline-list">
         <?php foreach ($results[SPOTIFY_CONTENT_TYPE::TRACK]->items as $result) { ?>
             <li>
-                <h1><?=$result->name?></h1>
                 <img width="300" height="300" src="<?=$result->album->images[0]->url?>">
+                <h1><?=$result->name?></h1>
             </li>
         <?php } ?>
     </ul>
