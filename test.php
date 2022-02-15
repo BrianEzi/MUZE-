d4a65353ec7a6d24fb18188c03de2268510e7ef6
n80569fh <faiad.hussain@student.manchester.ac.uk>
2022-02-15 12:42:05 +0000

    central search bar + basic colours

--- test.php
+++ test.php
@@ -7,12 +7,14 @@
     </head>
     <body>
         <div class="topnav">
+            <a class = "active" href="#home">HOME</a>
             <a href="#chat">CHAT</a>
             <a href="#games">GAMES</a>
             <a href="#yourmusic">YOUR MUSIC</a>
             <a href="#help">HELP</a>
         </div>
-        <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
-
+        <div class="box">
+            <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+        </div>
     </body>
 </html>
