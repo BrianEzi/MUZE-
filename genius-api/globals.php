214ebd2b0f1d72470f8a4489c5f677ad77e4176c
Asta Hanney <adil.hanney@student.manchester.ac.uk>
2022-03-01 20:58:51 +0000

    genius-api: add API client information

---
+++ genius-api/globals.php
@@ -0,0 +1,6 @@
+<?php
+
+const CLIENT_ID = "93qLZ131ftv7HAbc4IVYCFRqTbH7HW8xnVwAqi0BoDBA-tRV-QCEbkBuD9BvLlf5";
+const CLIENT_SECRET = "TUe3yVUyDqHbNUIWEd7z6V-yMeCD0CnPRV1t8x7MNdeCenBs35-dtnCBfVbxcynP3WD5ybb0zDxFPTnyby3Cww";
+const CLIENT_ACCESS_TOKEN = "XHuE1__CmDhW__MnQdCES6_UdsPtH0ypuvyeylqQiQiuq_NTeEhiISpBdDFWw6cw";
+
