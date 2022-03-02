373684fcc6da0b0fd2ed6590695b6df76f7a9fc5
Asta Hanney <adil.hanney@student.manchester.ac.uk>
2022-03-02 22:18:53 +0000

    lastfm-api: Add search function using last.fm

---
+++ lastfm-api/lastfm-api.php
@@ -0,0 +1,14 @@
+<?php
+require_once(__DIR__ . "/../all-apis/Requester.php");
+
+const API_KEY = "eeae367405a610b0a35e7c3864ce7a9d";
+const SHARED_SECRET = "b56530f5abb0aa5b7d851fa2dbb60673";
+
+class LastfmRequester extends BaseRequester {
+	public static string $URL_PREFIX = "http://ws.audioscrobbler.com/2.0";
+
+	protected static function pre_curl_request($curl, &$data) {
+		$data["api_key"] = API_KEY;
+		$data["format"] = "json";
+	}
+}
