b575a172a249e485a3b071ce909c4969354c2228
Jasveer Bains <jasbains2002@gmail.com>
2022-03-29 12:47:18 +0100

    accidently deleted this file

---
+++ discover.php
@@ -0,0 +1,375 @@
+<?php
+include_once(__DIR__ . "/genius-api/doSearch.php");
+require_once(__DIR__ . "/DBFunctions.php");
+@$searchTerm = $_GET["searchInput"];
+if (!empty($searchTerm)) {
+    // search with genius api
+	$results = doSearch($searchTerm, "5em");
+}
+?>
+
+<?php
+    session_start();
+    if (isset($_SESSION['background'])) {
+        $background = $_SESSION['background'];
+    } else {
+        $background = "assets/images/desert.jpg";
+    }
+
+    if (isset($_SESSION['tracks'])) {
+        $tracks = $_SESSION['tracks'];
+    } else {
+        $tracks = [];
+    }
+
+
+    if (isset($_SESSION['albums'])) {
+        $albums = $_SESSION['albums'];
+    } else {
+        $albums = [];
+    }
+
+    if (isset($_SESSION['artists'])) {
+        $artists = $_SESSION['artists'];
+    } else {
+        $artists = [];
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="assets/styles/myStyles.css">
+        <link rel="stylesheet" type="text/css" href="assets/styles/discoverStyleSheet.css">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MUZE# - Discover</title>
+
+        <script>
+            
+            document.addEventListener("DOMContentLoaded", function (event) {
+                var scrollPosition = localStorage.getItem("scrollPosition");
+                if (scrollPosition) {
+                    window.scrollTo(0, scrollPosition);
+                }
+            });
+
+            window.onscroll = function (e) {
+                localStorage.setItem("scrollPosition", window.scrollY);
+            };
+
+        </script>
+
+    </head>
+    <body style="background-image: url(<?php echo $background ?>);">
+        <div class="topnav">
+            <a href="home.php">HOME</a>
+            <a class = "active" href="discover.php">DISCOVER</a>
+            
+            <?php
+                if (isset($_SESSION['username'])) {
+                    echo '<a href="chat.php">CHAT</a>';
+                }
+            ?>
+            
+            <a href="games.php">GAMES</a>
+            
+            <?php
+
+            if (isset($_SESSION['username'])) {
+                $username = $_SESSION['username'];
+                echo'<a style="float: right;" href="myAccount.php">MY ACCOUNT</a>';
+            } else {
+                echo'<a style="float: right;" href="login.php">LOGIN</a>';
+            }
+
+            if (isset($_SESSION['username'])) {
+                $username = $_SESSION['username'];
+                echo'<a style="float: right;" href="myMusic.php">MY MUSIC</a>';
+            }
+            ?>
+        </div>
+
+        <div class="searchbar">
+            <form class="searchForm" method="get" action="discover.php">
+                <input id="searchInput" name="searchInput"
+                       placeholder="Find Music..." title="Type in a category"
+                       value="<?=$searchTerm?>">
+            </form>
+        </div>
+
+
+
+
+
+
+        <div class="content">
+        <?php
+        $resultIndex = 0;
+        if (!empty($searchTerm)) {
+            $tempIndex = 5;
+	        foreach (GENIUS_CONTENT_TYPE::$ALL as $type) {
+                // if the search results don't have any of this type, skip this type
+		        if (!array_key_exists($type, $results)) continue;
+
+		        $_SESSION['searchResults'] = $results[$type];
+                
+		        foreach ($results[$type] as $result) {
+
+                    ?>
+
+
+
+                    <form method="post">
+                            <?php
+
+                                if ($type == GENIUS_CONTENT_TYPE::SONG) {
+                                    echo '<input type="hidden" name="artist" value="' . $result["artist"] . '">';
+                                    
+                                }
+                                
+                                /*if ($type == GENIUS_CONTENT_TYPE::ALBUM) {
+                                    echo '<input type="hidden" name="artist" value="' . implode(", ", SearchComponent::getArtists($result)) . '">';
+                                }*/
+                                
+                                if ($type == GENIUS_CONTENT_TYPE::ARTIST) {
+                                    
+                                }
+                                ?>
+
+                            <input type="hidden" name="id" value="<?=$result["id"]?>">
+                            <input type="hidden" name="contentType" value="<?=strtoupper($result["type"])?>">
+                            <input type="hidden" name="title" value="<?=$result["name"]?>">
+                            <input type="hidden" name="image" value="<?=$result["biggest_image_url"]?>">
+
+                            <button type="submit" class="contentItem" name="expand">
+                            <!-- <div class="contentItem"> -->
+                                <div class="contentItem-image">
+                                    <?=$result["image_tag"]?>
+                                </div>
+                                <div class="contentItem-mainText">
+                                    <div class="contentLabel"><?=strtoupper($type)?></div>
+                                    <div class="title"><b><?=$result["name"]?></b></div>
+                                    <?php if ($type != GENIUS_CONTENT_TYPE::ARTIST) { ?>
+                                        <?=$result["artist"]?>
+                                    <?php } ?>
+                                </div>
+                                
+        
+                                <div class="contentIcons">
+        
+                                    <?php
+                                        if (isset($username) AND $type==GENIUS_CONTENT_TYPE::SONG) {
+
+                                    ?>
+
+                                    <form method="post" id="form" class="addForm">
+
+                                        <a type="submit" class="addButton" href="javascript:void(0)" onclick="document.getElementsByClassName('light')[<?=$resultIndex?>].style.display='flex';document.getElementById('fade').style.display='block'">
+                                            +
+                                        </a>
+
+                                    
+
+                                    <?php
+                                        } else if (isset($username)) {
+                                    ?>
+
+                                        <input type="hidden" name="index" value="<?=strval($resultIndex)?>">
+                                        <input type="hidden" name="submitted">
+                                        <input type="submit" class="inputSubmit" value="+">
+
+
+                                    <?php
+                                        }
+                                    ?>
+
+                                    </form>
+        
+                                </div>
+                            <!-- </div> -->
+        
+                        </button>
+                    </form>
+
+
+                    <div class="light">
+                        <div class="emptySpace"></div>
+
+                        <form class="playlistMenu" method="post">
+                        <input type="hidden" name="index" value="<?=strval($resultIndex)?>">
+                        <?php
+                            if (isset($_SESSION['playlists'])) {
+                                $playlists = $_SESSION['playlists'];
+                                foreach($playlists as $p) {
+                                    ?>
+                                        <button type="submit" name="playlistSubmitted" value="<?=$p[0]?>">
+                                            <?=$p[0]?>
+                                        </button>
+                                        <br><br>
+                                    <?php
+                                }
+                            }
+                        ?>
+                        </form>
+                        <br>
+                        <a class="closePopup" href="javascript:void(0)" onclick="document.getElementsByClassName('light')[<?=$resultIndex?>].style.display='none';document.getElementById('fade').style.display='none'">✕</a>
+                    </div>
+                        
+
+
+                    <?php
+                    $resultIndex += 1;
+                }
+        
+
+            }
+        }
+            
+            if (isset($_POST["playlistSubmitted"])) {
+                
+                $playlistName = $_POST['playlistSubmitted'];
+                $postIndex = intval($_POST["index"]);
+                
+                if ($postIndex < 19) {
+                    $postType = "TRACK";
+                    $geniusType = GENIUS_CONTENT_TYPE::SONG;
+                } else if ($postIndex < 39) {
+                    /*$postIndex -= 20;
+                    $postType = "ALBUM";
+	                $geniusType = GENIUS_CONTENT_TYPE::ALBUM;*/
+                } else {
+                    $postIndex -= 40;
+                    $postType = "ARTIST";
+	                $geniusType = GENIUS_CONTENT_TYPE::ARTIST;
+                }
+                echo $_POST['index'];
+                
+                // $resultToSave = ($results[$postType]->items)[$postIndex];
+
+                $resultToSave = ($results[$geniusType]->items)[$postIndex];
+                
+                
+                if ($postType == "TRACK") {
+
+                    if ($playlistName == "My Tracks") {
+                        addTrack($username, $resultToSave["name"], $resultToSave["artist"], $resultToSave["biggest_image_url"]);
+                        getTracks($username);
+                    } else {
+                        
+                        addToPlaylist($username, $playlistName, $resultToSave["name"], $resultToSave["artist"], $resultToSave["biggest_image_url"]);
+                        getPlaylists($username);
+                    }
+
+                }
+
+                /*if ($postType == GENIUS_CONTENT_TYPE::ALBUM) {
+                    addAlbum($username, $resultToSave["name"], $resultToSave["artist"], ["",""], $resultToSave["biggest_image_url"]);
+                    getAlbums($username);
+                }*/
+
+                if ($postType == GENIUS_CONTENT_TYPE::ARTIST) {
+                    addArtist($username, $resultToSave["name"], $resultToSave["biggest_image_url"]);
+                    getArtists($username);
+                }
+
+                unset($_POST["playlistSubmitted"]);
+                unset($_POST["index"]);
+                unset($_POST["type"]);
+
+
+
+                $_SESSION['reloadThePage'] = true;
+                echo "<meta http-equiv='refresh' content='0'>";
+            }
+
+
+
+
+            if (isset($_POST['submitted'])) {
+                $postIndex = intval($_POST["index"]);
+                
+                if ($postIndex < 19) {
+                    $postType = "TRACK";
+	                $geniusType = GENIUS_CONTENT_TYPE::SONG;
+                } else if ($postIndex < 39) {
+                    /*$postIndex -= 20;
+                    $postType = "ALBUM";
+	                $geniusType = GENIUS_CONTENT_TYPE::ALBUM;*/
+                } else {
+                    $postIndex -= 40;
+                    $postType = "ARTIST";
+	                $geniusType = GENIUS_CONTENT_TYPE::ARTIST;
+                }
+                echo $_POST['index'];
+                
+                // $resultToSave = ($results[$postType]->items)[$postIndex];
+
+                $resultToSave = ($results[$geniusType]->items)[$postIndex];
+
+                print_r($resultToSave);
+                
+                
+                if ($postType == "TRACK") {
+
+                    if ($playlistName == "My Tracks") {
+                        addTrack($username, $resultToSave["name"], $resultToSave["artist"], $resultToSave["biggest_image_url"]);
+                        getTracks($username);
+                    } else {
+                        
+                        addToPlaylist($username, $playlistName, $resultToSave["name"], $resultToSave["artist"], $resultToSave["biggest_image_url"]);
+                        getPlaylists($username);
+                    }
+
+                }
+
+                if ($postType == "ALBUM") {
+                    addAlbum($username, $resultToSave["name"], $resultToSave["artist"], ["",""], $resultToSave["biggest_image_url"]);
+                    getAlbums($username);
+                }
+
+                if ($postType == "ARTIST") {
+                    addArtist($username, $resultToSave["name"], $resultToSave["biggest_image_url"]);
+                    getArtists($username);
+                }
+
+                unset($_POST["index"]);
+                unset($_POST["type"]);
+
+
+
+                $_SESSION['reloadThePage'] = true;
+                echo "<meta http-equiv='refresh' content='0'>";
+            }
+
+
+
+
+            if (isset($_POST['expand'])) {
+                $_SESSION['title'] = $_POST['title'];
+                $_SESSION['image'] = $_POST['image'];
+                $_SESSION['type'] = $_POST['contentType'];
+                // $_SESSION['type'] = "TRACK";
+                if (isset($_POST['artist'])) {
+                    $_SESSION['artist'] = $_POST['artist'];
+                }
+                if ($_POST['contentType'] == "PLAYLIST") {
+                    foreach($_SESSION['playlists'] as $p) {
+                        if ($p[0] == $_POST['title']) {
+                            $_SESSION['tracklist'] = $p[1];
+                        }
+                    }
+                }
+                echo "<meta http-equiv='refresh' content='0;URL=contentInfo.php'>";
+            }
+
+        ?>
+        </div>
+
+        <div id="fade" class="black_overlay"></div>
+
+    </body>
+</html>
