daf6383f1c6b77e94ef3eb722fe51f461a952e7b
u68780be <brian.ezinwoke@student.manchester.ac.uk>
2022-03-30 12:57:32 +0100

    putting chat functions into Chat.php

--- internal-api/Chat.php
+++ internal-api/Chat.php
@@ -6,6 +6,18 @@ class Chat {
 	 */
 	public static function ListChats(): array {
 		// todo: use real data
+		$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+		if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+		}
+;		$sql = "SELECT * FROM ChatMessages WHERE friendshipID =" . $chatID;
+		$result = mysqli_query($conn, $sql);
+		if(mysqli_num_rows($result)>0){
+			return $result;
+		} else{
+			die("No messages found");
+		}
+		$conn->close();
 		return array(
 			"1000" => array(
 				"userName" => "John Doe",
@@ -32,6 +44,22 @@ class Chat {
 	public static function GetChatMessages(int $otherUserId): array {
 		$userId = $_SESSION["userId"];
 		// todo: use real data
+		$dbname = "2021_comp10120_m8";
+		$dbhost = "dbhost.cs.man.ac.uk";
+		$dbuser = "u68780be";
+		$dbpass = "Ezinwoke3-2022";
+		$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+		if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+		}
+;		$sql = "SELECT * FROM ChatMessages WHERE friendshipID =" . $chatID;
+		$result = mysqli_query($conn, $sql);
+		if(mysqli_num_rows($result)>0){
+			return $result;
+		} else{
+			die("No messages found");
+		}
+		$conn->close();
 		return array(
 			1000 => array(
 				"author" => $otherUserId,
